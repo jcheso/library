73f851111324128d11671140131d8477c0dceffd
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-04-19 20:35:35 +0800

    Added functionality to add and delete books

--- scripts.js
+++ scripts.js
@@ -16,9 +16,11 @@ addBookToLibrary("Twilight", "Meyers", "100", "No");
 
 console.table(myLibrary);
 
-function displayBooks(bookObj, index) {
+// Need to fix indexing
+function displayBook(bookObj, index) {
   console.log(bookObj);
   console.log(index);
+
 
   const bookShelf = document.querySelector("#book-shelf");
   const book = document.createElement("div");
@@ -28,23 +30,53 @@ function displayBooks(bookObj, index) {
 
   const bookTitle = document.createElement("p");
   bookTitle.setAttribute("class", "book-data");
-  bookTitle.textContent = bookObj.title
+  bookTitle.textContent = bookObj.title;
   book.appendChild(bookTitle);
 
   const bookAuthor = document.createElement("p");
   bookAuthor.setAttribute("class", "book-data");
-  bookAuthor.textContent = bookObj.author
+  bookAuthor.textContent = bookObj.author;
   book.appendChild(bookAuthor);
 
   const bookPages = document.createElement("p");
   bookPages.setAttribute("class", "book-data");
-  bookPages.textContent = bookObj.pages + ' Pages'
+  bookPages.textContent = bookObj.pages + " Pages";
   book.appendChild(bookPages);
 
   const bookRead = document.createElement("p");
   bookRead.setAttribute("class", "book-data");
-  bookRead.textContent = "Read? " + bookObj.haveRead
+  bookRead.textContent = "Read? " + bookObj.haveRead;
   book.appendChild(bookRead);
+
+  const bookRemove = document.createElement("button");
+  bookRemove.setAttribute("value", index);
+
+  bookRemove.addEventListener("click", deleteBook)
+  bookRemove.textContent = "Remove"
+  book.appendChild(bookRemove);
 }
 
-myLibrary.forEach(displayBooks);
+myLibrary.forEach(displayBook);
+
+// Submit function that saves form data, creates a new book object and displays it
+document.getElementById("add-book-form").addEventListener("submit", (event) => {
+  title = document.getElementById("title").value
+  author = document.getElementById("author").value;
+  pages = document.getElementById("pages").value;
+  haveRead = document.getElementById("haveRead").value;
+  if (haveRead !== "Yes") {
+    haveRead = "No";
+  }
+  addBookToLibrary(title, author, pages, haveRead);
+  displayBook(myLibrary[myLibrary.length - 1], myLibrary.length-1)
+  console.table(myLibrary)
+  event.preventDefault();
+});
+
+function deleteBook(){
+  let book = document.getElementById(this.value)
+  book.remove()
+  myLibrary.splice(this.value, 1, '');
+  console.table(myLibrary)
+
+}
